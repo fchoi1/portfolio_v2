25e8842c3baa3a15dae228f306b4b9bfbaf0ff79
fchoi1 <fchoi@outlook.com>
2024-04-16 19:34:30 -0400

    Intizl stuff

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import Header from "@/components/header";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fabio | Personal Portfolio",
+  description: "Fabio is a Full Stack Developer",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} h-[5000px] bg-gray-50 text-gray-950`}>
+        {/* dark:bg-[#946263] */}
+        {/* dark:bg-[#676394]" */}
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[1rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
+        <Header />
+
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { links } from "@/lib/data";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed left-1/2 flex justify-center items-center w-full -translate-x-1/2
+          rounded-none border-white border-opacity-40
+        bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem]
+          sm:top-6 sm:rounded-full sm:w-[36rem] sm:text-sm"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      >
+        <nav className="flex flex-wrap justify-around w-full sm:px-3 sm:py-1">
+          <ul className="flex flex-row flex-wrap justify-evenly center w-full gap-3 font-medium text-gray-500 sm:w-[inital] ">
+            {links.map((link, index) => (
+              <motion.li key={index} initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+                <Link
+                  className="flex w-full items-center justify-center p-3 hover:text-gray-950 transition"
+                  href={link.hash}
+                >
+                  {link.name}
+                </Link>
+              </motion.li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
+    </header>
+  );
+}
