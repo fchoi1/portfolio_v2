0827397fc6d4536bc9771bd4d6aa4efc7af1a96d
fchoi1 <fchoi@outlook.com>
2024-04-24 19:14:27 -0400

    fixing deplopy still

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current file's path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
     output: 'standalone',
      experimental: {
