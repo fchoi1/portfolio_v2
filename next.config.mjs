ec3fb9922ac86e44de408f2da9f2c0ab46386465
fchoi1 <fchoi@outlook.com>
2024-04-24 19:03:16 -0400

    fixing more

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+     experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 export default nextConfig;
