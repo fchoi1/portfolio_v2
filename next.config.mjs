4c9009f3073fe18394341cc57830faafe67e2e41
fchoi1 <fchoi@outlook.com>
2024-04-24 20:10:10 -0400

    fixed build for standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current file's path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const nextConfig = {
     output: 'standalone',
+    experimental: {
+        // this includes files from the monorepo base two directories up
+        outputFileTracingIncludes: {  "/": ["/app"]}
+    },
 };
 
 export default nextConfig;
